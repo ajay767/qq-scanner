ed64a2b0feeaedb236800d45c0748329469b7b60
AJAY YADAV <58135870+ajay767@users.noreply.github.com>
2020-11-01 20:02:55 +0530

    pushing original codes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
-const app = express();
 
+const app = express();
 app.use(express.static(path.join(__dirname + "/public")));
 
 app.get("/", (req, res) => {
--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,2 @@
-import QrScanner from "qr-scanner";
-
-const videoElem = document.querySelector("video");
-const qrScanner = new QrScanner(videoElem, (result) =>
-  console.log("decoded qr code:", result)
-);
-
-QrScanner.hasCamera()
-  .then(() => {
-    console.log("yess it has");
-  })
-  .catch(() => {
-    console.log("no camera found");
-  });
-qrScanner.start();
+import qrGenerator from "./qr-generator";
+import qrScanner from "./qr-scanner";
